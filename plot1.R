7e6e361efb4857ba60baff847d9a24e54df40ff1
BAKINTU <blessinglovet1@gmail.com>
2022-04-14 22:01:17 -0400

    Plot1

---
+++ plot1.R
@@ -0,0 +1,16 @@
+data <- read.table("household_power_consumption.txt", 
+                   header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data_sub <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+datetime <- paste(as.Date(data_sub$Date), data_sub$Time)
+data_sub$Datetime <- as.POSIXct(datetime)
+
+hist(data_sub$global_activepower, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", 
+     ylab="Frequency", col="Red")
+
+
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
